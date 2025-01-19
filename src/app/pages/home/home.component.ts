7cd07e4f771eb1a072db73763314ac1dc29e0495
adisunjana <adisunjana443@gmail.com>
2025-01-19 11:47:19 +0700

    update home and integrasi

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,76 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    FormsModule, ReactiveFormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  carList: any[] = []
+  idModal: any = '';
+  selectedOrder: any;
+  totalPrice: any;
+  totalDays: any;
+
+  startDate: any;
+  endDate: any;
+
+  baseUrl: string = 'http://localhost:8000/api'
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get(this.baseUrl + '/cars').subscribe(
+      (res: any) => {
+        this.carList = res?.data
+        console.log(this.carList)
+      }
+    )
+  }
+
+  order(item: any) {
+    console.log(item)
+    this.selectedOrder = item;
+  }
+
+  doOrder(item: any) {
+    //
+    this.http.post(this.baseUrl + '/rentals', {
+      "car_id": this.selectedOrder.id,
+      "user_id": "1",
+      "start_date": new Date(this.startDate),
+      "end_date": new Date(this.endDate),
+    }
+    ).subscribe(res => {
+      console.log(res)
+    })
+  }
+
+  calculate(start: any, end: any) {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    const timeDiff = endDate.getTime() - startDate.getTime();
+    const result = Math.ceil(timeDiff / (1000 * 3600 * 24)); // Menghitung jumlah hari
+    const price = Number(this.selectedOrder.rental_price_per_day)
+    if (!Number.isNaN(result)) {
+      this.totalPrice = result * price
+      this.totalDays = result
+      console.log(this.totalPrice)
+    }
+
+  }
+
+  closeModal() {
+    this.totalDays = null;
+    this.totalPrice = null;
+    this.startDate = null;
+    this.endDate = null;
+  }
 
 }
